e03b6167d62b8fa8d3478ab4a9b23ec9bcb9fb76
sh4rkd <20006132@es.uveg.edu.mx>
2022-11-29 06:48:34 -0700

    creando modulo main con configuraciones de la db y prueba de hello world

---
+++ todo/__init__.py
@@ -0,0 +1,18 @@
+import os 
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='mikey',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE')
+    )
+    
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app

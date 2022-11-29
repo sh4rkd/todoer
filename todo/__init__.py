c8bd1a521ed3fa27543a5baa32e20d7fa8b7ec65
sh4rkd <20006132@es.uveg.edu.mx>
2022-11-29 07:59:05 -0700

    iniciando autenticación con password encriptada

--- todo/__init__.py
+++ todo/__init__.py
@@ -14,6 +14,9 @@ def create_app():
     from . import db
     db.init_app(app)
     
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
---
+++ todo/auth.py
@@ -0,0 +1,34 @@
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from todo.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+            'SELECT id FROM user WHERE username = %s'
+        )
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif c.fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+        if error is None:
+            c.execute(
+                'INSERT INTO user (username, password) VALUES (%s, %s)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')

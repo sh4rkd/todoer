bcf2acce5989ce63e6d5ff270f0696bf05c8e7f7
sh4rkd <20006132@es.uveg.edu.mx>
2022-11-29 19:59:10 -0700

    listando todos

--- todo/auth.py
+++ todo/auth.py
@@ -51,7 +51,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
         flash(error)
     return render_template('auth/login.html')
 

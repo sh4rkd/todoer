95757688b4def5cfa08aeedad984664497244275
sh4rkd <20006132@es.uveg.edu.mx>
2022-12-04 11:29:11 -0700

    terminando update, db, form, conexion etc..

--- todo/todo.py
+++ todo/todo.py
@@ -42,9 +42,42 @@ def create():
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
+    todo = get_todo(id)
+    if request.method == 'POST':
+        description = request.form['description']
+        completed = True if request.form.get('completed') else False
+        error = None
+
+        if not description:
+            error = 'Description is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'UPDATE todo SET description = %s, completed = %s WHERE id = %s',
+                (description, completed, id)
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
     return render_template('todo/update.html', todo=todo)
 
-@bp.route('/<int>:id/delete', methods=['POST'])
+@bp.route('/<int:id>/delete', methods=['POST'])
 @login_required
 def delete():
     return ""
+
+def get_todo(id):
+    db, c = get_db()
+    c.execute(
+        """
+        select t.id, t.description, t.completed, t.created_by, t.created_at, u.username
+        from todo t JOIN user u on t.created_by = u.id where t.id = %s
+        """
+        , (id,)
+    )
+    todo = c.fetchone()
+    if todo is None:
+        abort(404, "Todo id {0} doesn't exist.".format(id))
+    return todo
